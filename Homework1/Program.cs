c3f8ca94a2e5a6b8b1f1d29c288420182790560b
Pavel <fadeevpavel181@gmail.com>
2022-11-30 21:24:42 +0300

    Task 1.6

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -26,7 +26,7 @@ else
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-
+/*
 Console.Clear();
 Console.Write("Введите первое целое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
@@ -41,3 +41,20 @@ if(num2 > max) max = num2;
 if(num3 > max) max = num3;
 
 Console.WriteLine(max);
+*/
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+
+Console.Clear();
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.WriteLine($"Число {num} четное.");
+}
+else
+{
+    Console.WriteLine($"Число {num} нечетное.");
+}
