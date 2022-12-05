f26c283197cb25d333444efb3ad74782960ff234
Pavel <fadeevpavel181@gmail.com>
2022-12-05 18:09:34 +0300

    Homework2 task 2/10

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int FindNumber (int num)
+{
+    int result = num / 10 % 10;
+    return result;
+}
+
+Console.Clear();
+
+Console.Write("Ведите целое трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int secondnum = FindNumber(num);
+
+Console.WriteLine (secondnum);
