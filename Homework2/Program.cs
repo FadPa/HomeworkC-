71fd9c716c11d3940cdc60d3e3a427c19451bf6e
Pavel <fadeevpavel181@gmail.com>
2022-12-05 18:26:12 +0300

    Homework2 task 2.13

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,9 +1,6 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
-
+/*
 int FindNumber (int num)
 {
     int result = num / 10 % 10;
@@ -17,3 +14,31 @@ int num = Convert.ToInt32(Console.ReadLine());
 int secondnum = FindNumber(num);
 
 Console.WriteLine (secondnum);
+*/
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+int SearchThirdNumber (int num)
+{
+    while (num > 1000)
+    {
+        num = num / 10;
+    }
+    int result = num % 10;
+    return result;
+}
+
+Console.Clear();
+Console.Write("Ведите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100)
+{
+    int thirdnumber = SearchThirdNumber(num);
+    Console.WriteLine(thirdnumber);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
