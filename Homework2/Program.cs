2caebba0fc5b4023ef14edfa45288e6562d6f752
Pavel <fadeevpavel181@gmail.com>
2022-12-05 18:42:39 +0300

    Homework2 task 2.15

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -18,7 +18,7 @@ Console.WriteLine (secondnum);
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-
+/*
 int SearchThirdNumber (int num)
 {
     while (num > 1000)
@@ -42,3 +42,31 @@ else
 {
     Console.WriteLine("Третьей цифры нет.");
 }
+*/
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+void DayWeek (int day)
+{
+    if(day == 6 || day ==7)
+    {
+        Console.WriteLine("Данный день недели является выходным");
+    }
+    else
+    {
+        if(day > 7 || day < 1)
+        {
+             Console.WriteLine("Данное число не является днём недели");
+        }
+        else
+        {
+            Console.WriteLine("Данный день недели не является выходным");
+        }
+    }
+}
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+DayWeek(dayNumber);
