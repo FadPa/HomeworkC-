9f34d82522bd6564c199ac74435bc3f164507688
Pavel <fadeevpavel181@gmail.com>
2022-12-17 16:22:07 +0300

    Homework6.41

--- Homework6/Program.cs
+++ Homework6/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void CountNumber (int numM)
+{
+    int count = 0;
+    for (int i = 0; i < numM; i++)
+    {
+        Console.Write("Введите число: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if(num > 0) count++;
+        Console.WriteLine($"Количество введённых чисел больше 0 равняется {count}");
+    }
+    
+}
+
+Console.Clear();
+Console.Write("Введите количество вводимых чисел: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+CountNumber(num);
