27ff8257b27892dd32684047222fcdc87d5ba697
Pavel <fadeevpavel181@gmail.com>
2022-12-27 15:04:43 +0300

    Homework8.56

--- Homework8/Program.cs
+++ Homework8/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 // Например, задан массив:
-
+/*
 int[,] CreateRandom2dArray()
 {
     Console.Write("Input a number of rows: ");
@@ -60,3 +60,72 @@ int[,] myArray = CreateRandom2dArray();
 Show2dArray(myArray);
 OrderArrayLines(myArray);
 Show2dArray(myArray);
+*/
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Input a number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j= 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void MinString(int[,] array)
+{
+    int min  = 0;
+    int sum = SumString(array, 0);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int temp = SumString(array, i);
+        if (sum > temp)
+        {
+            sum = temp;
+            min = i+1;
+        }
+    }
+    Console.WriteLine($"Строка {min} с минимальной суммой элементов равной {sum}");
+}
+
+int SumString(int[,] array, int i)
+{
+    int sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        sum += array[i,j];
+    } 
+    return sum;
+}
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+MinString(myArray);
