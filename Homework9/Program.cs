0dd019dae67c049d7b419833a8c1d3857585ba06
Pavel <fadeevpavel181@gmail.com>
2022-12-28 21:06:18 +0300

    Homework9.64

--- Homework9/Program.cs
+++ Homework9/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void ShowNums(int num)
+{
+    Console.Write(num + " ");
+    if(num > 1) ShowNums(num - 1);
+    
+}
+
+Console.Write("Input a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+ShowNums(n);
